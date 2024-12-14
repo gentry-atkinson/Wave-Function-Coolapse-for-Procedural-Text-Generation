41529861b9646c10c2de909f20f245532107fee2
Gentry Atkinson <gatkinso@stedwards.edu>
2024-12-14 11:41:11 -0600

    Fitting to 10 texts now

--- main.py
+++ main.py
@@ -19,37 +19,16 @@ except:
     nltk.download('punkt_tab')
 
 if __name__ == '__main__':
-    text_idx = 0
-    book_text = open(os.path.join('text', text_list[text_idx])).read()
-
-    book_text = book_text.replace('\r', '')
-    book_text = book_text.replace('\n', '')
-    sentences = nltk.tokenize.sent_tokenize(book_text)
-
-
     wt = WaveText(max_dist=3)
-    print(f'Fitting to text: {text_list[text_idx]}')
-    wt.fit(sentences)
 
-    text_idx = 1
-    book_text = open(os.path.join('text', text_list[text_idx])).read()
+    for text_idx, text_name in enumerate(text_list):
+        book_text = open(os.path.join('text', text_list[text_idx])).read()
 
-    book_text = book_text.replace('\r', '')
-    book_text = book_text.replace('\n', '')
-    sentences = nltk.tokenize.sent_tokenize(book_text)
-
-    print(f'Fitting to text: {text_list[text_idx]}')
-    wt.fit(sentences)
-
-    text_idx = 2
-    book_text = open(os.path.join('text', text_list[text_idx])).read()
-
-    book_text = book_text.replace('\r', '')
-    book_text = book_text.replace('\n', '')
-    sentences = nltk.tokenize.sent_tokenize(book_text)
-
-    print(f'Fitting to text: {text_list[text_idx]}')
-    wt.fit(sentences)
+        book_text = book_text.replace('\r', '')
+        book_text = book_text.replace('\n', '')
+        sentences = nltk.tokenize.sent_tokenize(book_text)
+        print(f'Fitting to text: {text_name}')
+        wt.fit(sentences)
     
     sample_output = wt.generate("all people", str_len=12)
     print(sample_output)
